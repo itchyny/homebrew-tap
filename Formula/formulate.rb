d776963d2de6c1bc703a18597a75ba27ad3fc8a7
itchyny <itchyny@hatena.ne.jp>
2019-03-21 00:52:52 +0900

    formulate v0.0.3

--- Formula/formulate.rb
+++ Formula/formulate.rb
@@ -1,11 +1,12 @@
 class Formulate < Formula
   homepage 'https://github.com/itchyny/formulate'
-  version '0.0.2'
-  url 'https://github.com/itchyny/formulate/archive/v0.0.2.zip'
-  sha256 '288759b2c47566687ecac93590d360c1a0068a9a12af7f27b3c472e82461e83a'
+  version '0.0.3'
+  url 'https://github.com/itchyny/formulate/archive/v0.0.3.zip'
+  sha256 '129522441f4eb3e7f52df979e04dd129dc86c48444027b3e6325ba21188a722d'
 
   def install
     bin.install 'formulate'
+    zsh_completion.install "_formulate"
   end
 
   test do
--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Homebrew taps for itchyny's projects
 
 - [fillin](https://github.com/itchyny/fillin) 0.1.0
-- [formulate](https://github.com/itchyny/formulate) 0.0.2
+- [formulate](https://github.com/itchyny/formulate) 0.0.3
 - [git-branch-name](https://github.com/itchyny/git-branch-name) 0.0.1
 - [gojo](https://github.com/itchyny/gojo) 0.0.4
 - [maze](https://github.com/itchyny/maze) 0.0.3
