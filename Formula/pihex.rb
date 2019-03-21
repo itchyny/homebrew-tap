ae24fcaf6921815789e917b4aaddddc83442e7f2
itchyny <itchyny@hatena.ne.jp>
2019-03-21 22:29:43 +0900

    remove head from pihex formula

--- Formula/pihex.rb
+++ Formula/pihex.rb
@@ -3,7 +3,6 @@ class Pihex < Formula
   version '0.1.3'
   url 'https://github.com/itchyny/pihex-rs/releases/download/v0.1.3/pihex_darwin.zip'
   sha256 '257c740f408426f32c931214b59bd53bb45005cda9974b8c77a4aeb22ded91d7'
-  head 'https://github.com/itchyny/pihex-rs.git'
 
   def install
     bin.install 'pihex'
