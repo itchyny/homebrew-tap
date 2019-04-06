5b9eb7b35bf9a9dd4fd96bc9eb9756c897234b2d
itchyny <itchyny@hatena.ne.jp>
2019-04-07 01:33:36 +0900

    formulate v0.0.5

--- Formula/formulate.rb
+++ Formula/formulate.rb
@@ -1,8 +1,8 @@
 class Formulate < Formula
   homepage 'https://github.com/itchyny/formulate'
-  version '0.0.4'
-  url 'https://github.com/itchyny/formulate/archive/v0.0.4.zip'
-  sha256 'b53cc1c438960d272d6fe7f22c845fdd03724f90adfe41a74fc814b557d90e83'
+  version '0.0.5'
+  url 'https://github.com/itchyny/formulate/archive/v0.0.5.zip'
+  sha256 'df9f94d77200f595930728039ea213f4c91030899d96abadd8ee903fbfc77427'
 
   def install
     bin.install 'formulate'
--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 
 - [base58](https://github.com/itchyny/base58-go) 0.0.3
 - [fillin](https://github.com/itchyny/fillin) 0.1.1
-- [formulate](https://github.com/itchyny/formulate) 0.0.4
+- [formulate](https://github.com/itchyny/formulate) 0.0.5
 - [git-branch-name](https://github.com/itchyny/git-branch-name) 0.0.1
 - [gojo](https://github.com/itchyny/gojo) 0.0.7
 - [maketen](https://github.com/itchyny/maketen-go) 0.0.1
