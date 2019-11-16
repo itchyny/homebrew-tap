6dec9b36fa5961192344de55f1e75b02ebea217d
itchyny <itchyny@hatena.ne.jp>
2019-11-17 00:16:14 +0900

    maze v0.0.6

--- Formula/maze.rb
+++ Formula/maze.rb
@@ -1,8 +1,8 @@
 class Maze < Formula
   homepage 'https://github.com/itchyny/maze'
-  version '0.0.5'
-  url 'https://github.com/itchyny/maze/releases/download/v0.0.5/maze_v0.0.5_darwin_amd64.zip'
-  sha256 'a424ef0f090f3f1e7f335894e7b61956d4c01dc15330576523a5caf3dc8c32f2'
+  version '0.0.6'
+  url 'https://github.com/itchyny/maze/releases/download/v0.0.6/maze_v0.0.6_darwin_amd64.zip'
+  sha256 'cc26db0a82cfe6d4f3a0c112f36e5402dc590f5858b2339e91bd0d00e864e333'
 
   head do
     url 'https://github.com/itchyny/maze.git'
