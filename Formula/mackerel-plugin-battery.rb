edb69e5b42b8e39d0c024da415b3c508778d97a0
itchyny <itchyny@hatena.ne.jp>
2019-12-09 20:42:40 +0900

    mackerel-plugin-battery v0.0.3

--- Formula/mackerel-plugin-battery.rb
+++ Formula/mackerel-plugin-battery.rb
@@ -1,8 +1,8 @@
 class MackerelPluginBattery < Formula
   homepage 'https://github.com/itchyny/mackerel-plugin-battery'
-  version '0.0.2'
-  url 'https://github.com/itchyny/mackerel-plugin-battery/releases/download/v0.0.2/mackerel-plugin-battery_v0.0.2_darwin_amd64.zip'
-  sha256 'f16c7c02cb5926f0b3a8410a0e6599e6cdf244c8ac4221b2b1b812120f5643df'
+  version '0.0.3'
+  url 'https://github.com/itchyny/mackerel-plugin-battery/releases/download/v0.0.3/mackerel-plugin-battery_v0.0.3_darwin_amd64.zip'
+  sha256 'd0fd666bbbb6613efb670980b4a10075ec636d446e66ece8885f99471d192e59'
 
   head do
     url 'https://github.com/itchyny/mackerel-plugin-battery.git'
