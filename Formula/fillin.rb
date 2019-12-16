b9d89704e29fa1fe9be45cf50940e48bd6c8535d
itchyny <itchyny@hatena.ne.jp>
2019-12-16 23:56:09 +0900

    fillin v0.2.0

--- Formula/fillin.rb
+++ Formula/fillin.rb
@@ -1,8 +1,8 @@
 class Fillin < Formula
   homepage 'https://github.com/itchyny/fillin'
-  version '0.1.1'
-  url 'https://github.com/itchyny/fillin/releases/download/v0.1.1/fillin_darwin_amd64.zip'
-  sha256 '5fbab2e595b3cebafa2309c47356ee788483495541fedc829009a2cad9d8eb32'
+  version '0.2.0'
+  url 'https://github.com/itchyny/fillin/releases/download/v0.2.0/fillin_v0.2.0_darwin_amd64.zip'
+  sha256 'bc6a8ed61d5750d39a6fadc1eed5b695da99e4619067c3b311e1deedb069d9d0'
 
   head do
     url 'https://github.com/itchyny/fillin.git'
@@ -12,10 +12,8 @@ class Fillin < Formula
   def install
     if build.head?
       system 'make', 'build'
-      bin.install 'build/fillin'
-    else
-      bin.install 'fillin'
     end
+    bin.install 'fillin'
   end
 
   test do
