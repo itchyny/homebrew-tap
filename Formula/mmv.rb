d93b9b8a1009347183632d0295cf9e45ea59847f
itchyny <itchyny@hatena.ne.jp>
2020-01-09 21:42:00 +0900

    mmv v0.1.1

--- Formula/mmv.rb
+++ Formula/mmv.rb
@@ -1,8 +1,8 @@
 class Mmv < Formula
   homepage 'https://github.com/itchyny/mmv'
-  version '0.1.0'
-  url 'https://github.com/itchyny/mmv/releases/download/v0.1.0/mmv_v0.1.0_darwin_amd64.zip'
-  sha256 '3db7c868d9600547f091a07f072297977ea109f9cca53218863be9a8ef3c98cb'
+  version '0.1.1'
+  url 'https://github.com/itchyny/mmv/releases/download/v0.1.1/mmv_v0.1.1_darwin_amd64.zip'
+  sha256 '8c2e62795760fa184adee5734f3e0bca85738b13adfa067b212da72f7560cd3f'
 
   head do
     url 'https://github.com/itchyny/mmv.git'
