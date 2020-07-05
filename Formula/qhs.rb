3efabb783125cced0d7d178636d07940538a4f9a
itchyny <itchyny@hatena.ne.jp>
2020-07-06 00:49:23 +0900

    qhs v0.3.1

--- Formula/qhs.rb
+++ Formula/qhs.rb
@@ -1,11 +1,12 @@
 class Qhs < Formula
   homepage 'https://github.com/itchyny/qhs'
-  version '0.3.0'
-  url 'https://github.com/itchyny/qhs/releases/download/v0.3.0/qhs_v0.3.0_darwin_amd64.zip'
-  sha256 '1d7d9bce58a7e8df3e12a2e99b3e65175c977971bfe607bc7dbf722edb42d0b7'
+  version '0.3.1'
+  url 'https://github.com/itchyny/qhs/releases/download/v0.3.1/qhs_v0.3.1_darwin_amd64.zip'
+  sha256 '156e79e7a3f5ac36c5dc92658753b2e7a0b98c08c720813d041b9c99fea55908'
 
   def install
     bin.install 'qhs'
+    zsh_completion.install "_qhs"
   end
 
   test do
