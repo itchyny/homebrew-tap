bb274cf12b201901d0d2be7c4db49d5f6432fe90
itchyny <itchyny@hatena.ne.jp>
2020-07-13 22:49:32 +0900

    fillin v0.3.1

--- Formula/fillin.rb
+++ Formula/fillin.rb
@@ -1,8 +1,8 @@
 class Fillin < Formula
   homepage 'https://github.com/itchyny/fillin'
-  version '0.3.0'
-  url 'https://github.com/itchyny/fillin/releases/download/v0.3.0/fillin_v0.3.0_darwin_amd64.zip'
-  sha256 'db349be77a35da6e91511edc8b11fb624d0cf80b32756863c48b81f675e36bdb'
+  version '0.3.1'
+  url 'https://github.com/itchyny/fillin/releases/download/v0.3.1/fillin_v0.3.1_darwin_amd64.zip'
+  sha256 '5fedafb6052b7ace35b68568598fa017737b050eb5a67e7ce614a98d73bbfdea'
 
   head do
     url 'https://github.com/itchyny/fillin.git'
--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 
 - [base58](https://github.com/itchyny/base58-go) 0.1.0
 - [bed](https://github.com/itchyny/bed) 0.2.0
-- [fillin](https://github.com/itchyny/fillin) 0.3.0
+- [fillin](https://github.com/itchyny/fillin) 0.3.1
 - [formulate](https://github.com/itchyny/formulate) 0.0.6
 - [git-branch-name](https://github.com/itchyny/git-branch-name) 0.0.1
 - [gojo](https://github.com/itchyny/gojo) 0.1.1
