3a74dcdf66864160d1b3c9e22f2f34f402cec628
itchyny <itchyny@hatena.ne.jp>
2020-08-22 21:24:48 +0900

    gojq v0.11.1

--- Formula/gojq.rb
+++ Formula/gojq.rb
@@ -1,13 +1,13 @@
 class Gojq < Formula
   homepage 'https://github.com/itchyny/gojq'
-  version '0.11.0'
+  version '0.11.1'
 
   if OS.mac?
-    url 'https://github.com/itchyny/gojq/releases/download/v0.11.0/gojq_v0.11.0_darwin_amd64.zip'
-    sha256 'dab8b30378e926d18848e0d98edc56855263da15c456ead8f7e1fd609176d3fa'
+    url 'https://github.com/itchyny/gojq/releases/download/v0.11.1/gojq_v0.11.1_darwin_amd64.zip'
+    sha256 '5d494770e6299303dda90e7faaf7e9b4dd03e5813609d154f2544e26e7cf1a38'
   elsif OS.linux?
-    url 'https://github.com/itchyny/gojq/releases/download/v0.11.0/gojq_v0.11.0_linux_amd64.tar.gz'
-    sha256 'f2093521e728292cd3f3f5265136a3b20bb628bd021240e75a317a8c4a22ee58'
+    url 'https://github.com/itchyny/gojq/releases/download/v0.11.1/gojq_v0.11.1_linux_amd64.tar.gz'
+    sha256 '42a5ba7fbfc082285fff7a326018a9c979105377420554a3af2308aa86681f28'
   end
 
   head do
