59a37f772547fdbe6212bf5c2880bf2cf22ceeed
itchyny <itchyny@hatena.ne.jp>
2020-10-03 22:45:13 +0900

    qhs v0.3.3

--- Formula/qhs.rb
+++ Formula/qhs.rb
@@ -1,13 +1,13 @@
 class Qhs < Formula
   homepage 'https://github.com/itchyny/qhs'
-  version '0.3.2'
+  version '0.3.3'
 
   if OS.mac?
-    url 'https://github.com/itchyny/qhs/releases/download/v0.3.2/qhs_v0.3.2_darwin_amd64.zip'
-    sha256 '14d27415989c92a383d981142cf98899f0fdcbcc8b63dbc99ad90b9b51892df8'
+    url 'https://github.com/itchyny/qhs/releases/download/v0.3.3/qhs_v0.3.3_darwin_amd64.zip'
+    sha256 'f833a75462ead44d80a985e2f8246e496c357cb30a40b14decc129742cc38bcb'
   elsif OS.linux?
-    url 'https://github.com/itchyny/qhs/releases/download/v0.3.2/qhs_v0.3.2_linux_amd64.zip'
-    sha256 '311a1ee49a68d3dbf29c7a9eda4f7400f3285718b9d7943e99efd7072189733d'
+    url 'https://github.com/itchyny/qhs/releases/download/v0.3.3/qhs_v0.3.3_linux_amd64.zip'
+    sha256 '8f5371fd34ecc3205692994c3df4a1f16bfd499cb679161ad7ad1b4606a191c1'
   end
 
   def install
--- README.md
+++ README.md
@@ -17,7 +17,7 @@
 - [mkrg](https://github.com/itchyny/mkrg) 0.0.3
 - [mmv](https://github.com/itchyny/mmv) 0.1.2
 - [pihex](https://github.com/itchyny/pihex-rs) 0.1.5
-- [qhs](https://github.com/itchyny/qhs) 0.3.2
+- [qhs](https://github.com/itchyny/qhs) 0.3.3
 - [rexdep](https://github.com/itchyny/rexdep) 0.3.0
 - [sjsp](https://github.com/itchyny/sjsp) 0.1.1
 - [volume](https://github.com/itchyny/volume-go) 0.2.0
