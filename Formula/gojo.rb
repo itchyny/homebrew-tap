122fcb3cba28d1cd8051fd51f1fb8e4564c0ff67
itchyny <itchyny@hatena.ne.jp>
2021-01-10 09:35:51 +0900

    gojo v0.1.2

--- Formula/gojo.rb
+++ Formula/gojo.rb
@@ -1,13 +1,13 @@
 class Gojo < Formula
   homepage 'https://github.com/itchyny/gojo'
-  version '0.1.1'
+  version '0.1.2'
 
   if OS.mac?
-    url 'https://github.com/itchyny/gojo/releases/download/v0.1.1/gojo_v0.1.1_darwin_amd64.zip'
-    sha256 '762015808efee6af28133c545d1e6058c018eca9fcee379a0033a2b3370dec70'
+    url 'https://github.com/itchyny/gojo/releases/download/v0.1.2/gojo_v0.1.2_darwin_amd64.zip'
+    sha256 '62b4ca1aaa2fa92be553535f7700a18ab597e06322569473c1846e51f26fd0af'
   elsif OS.linux?
-    url 'https://github.com/itchyny/gojo/releases/download/v0.1.1/gojo_v0.1.1_linux_amd64.tar.gz'
-    sha256 '06174e2c8efc3bf0372a4a7402a8f9ea509e9cd60257c553453aafbb025cf65f'
+    url 'https://github.com/itchyny/gojo/releases/download/v0.1.2/gojo_v0.1.2_linux_amd64.tar.gz'
+    sha256 'aebd7a490e93ef76486fe48e5cbc7ee2a504ffcb7899bdd0bc6591d4c03c74ef'
   end
 
   head do
