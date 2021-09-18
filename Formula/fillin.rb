7c7475a323a51b31e9fc0af3542db6d6d2bc2aeb
itchyny <itchyny@hatena.ne.jp>
2021-09-18 23:55:51 +0900

    fillin v0.3.3

--- Formula/fillin.rb
+++ Formula/fillin.rb
@@ -1,13 +1,23 @@
 class Fillin < Formula
   homepage 'https://github.com/itchyny/fillin'
-  version '0.3.2'
+  version '0.3.3'
 
   if OS.mac?
-    url 'https://github.com/itchyny/fillin/releases/download/v0.3.2/fillin_v0.3.2_darwin_amd64.zip'
-    sha256 '72e0dab9b213350c627a8701dda62eca6b8a90bafde8ce0d4c525fbf7d98e441'
+    if Hardware::CPU.arm?
+      url 'https://github.com/itchyny/fillin/releases/download/v0.3.3/fillin_v0.3.3_darwin_arm64.zip'
+      sha256 '12a373d8d1a3d97b8a3c7f40e6ef85306ec23f8750d962ce6d9c079f2f6fe8e7'
+    elsif Hardware::CPU.intel?
+      url 'https://github.com/itchyny/fillin/releases/download/v0.3.3/fillin_v0.3.3_darwin_amd64.zip'
+      sha256 'c26a9a366541c4d7135b3b80b9f86bc874e3f8a92c6b31a000490028c2258ffc'
+    end
   elsif OS.linux?
-    url 'https://github.com/itchyny/fillin/releases/download/v0.3.2/fillin_v0.3.2_linux_amd64.tar.gz'
-    sha256 'b093b2d720773963c394a81a803c013c13130e824cbf82efd336a01529a5b5fb'
+    if Hardware::CPU.arm?
+      url 'https://github.com/itchyny/fillin/releases/download/v0.3.3/fillin_v0.3.3_linux_arm64.tar.gz'
+      sha256 'c5c7e472fea2a8d47009d6d29c17ed35289e4c097fece018016f847191c425a0'
+    elsif Hardware::CPU.intel?
+      url 'https://github.com/itchyny/fillin/releases/download/v0.3.3/fillin_v0.3.3_linux_amd64.tar.gz'
+      sha256 '36a8e39407bc0cadbcc155e07ed2dcb503c7d9df4c596f135c7f6a64a2c31d11'
+    end
   end
 
   head do
