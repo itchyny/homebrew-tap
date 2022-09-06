2280c7fcc7bff57c199b682bd25a3b224e921f36
itchyny <itchyny@cybozu.co.jp>
2022-09-06 20:31:17 +0900

    json2yaml v0.1.3

--- Formula/json2yaml.rb
+++ Formula/json2yaml.rb
@@ -1,22 +1,22 @@
 class Json2yaml < Formula
   homepage 'https://github.com/itchyny/json2yaml'
-  version '0.1.2'
+  version '0.1.3'
 
   if OS.mac?
     if Hardware::CPU.arm?
-      url 'https://github.com/itchyny/json2yaml/releases/download/v0.1.2/json2yaml_v0.1.2_darwin_arm64.zip'
-      sha256 '9f73002f5f872e033081745c67318d3252a3dc68f30048f98cfb0a3fa435c05a'
+      url 'https://github.com/itchyny/json2yaml/releases/download/v0.1.3/json2yaml_v0.1.3_darwin_arm64.zip'
+      sha256 '1f18e00fb481799d1a2c72d76396ed4f7d99c374802a063ecbbcde943f8f075c'
     elsif Hardware::CPU.intel?
-      url 'https://github.com/itchyny/json2yaml/releases/download/v0.1.2/json2yaml_v0.1.2_darwin_amd64.zip'
-      sha256 '82ea5c9a790a362439b62702b6438fc7bb99730b9eb5e87249450c43f17baf69'
+      url 'https://github.com/itchyny/json2yaml/releases/download/v0.1.3/json2yaml_v0.1.3_darwin_amd64.zip'
+      sha256 '12dbb82ef56509f75338487c650a1102ac45368fc4f6be946cdfc12e9b50e1d8'
     end
   elsif OS.linux?
     if Hardware::CPU.arm?
-      url 'https://github.com/itchyny/json2yaml/releases/download/v0.1.2/json2yaml_v0.1.2_linux_arm64.tar.gz'
-      sha256 '5aaa9b7c4ef06df406940931314c6a31127bc6d15fd62286e14b388ea49a6e2c'
+      url 'https://github.com/itchyny/json2yaml/releases/download/v0.1.3/json2yaml_v0.1.3_linux_arm64.tar.gz'
+      sha256 '01b2fc4121d061f38d31359180c4eb0b8494a0c37cf6bd65fd8924f9f1bf2e43'
     elsif Hardware::CPU.intel?
-      url 'https://github.com/itchyny/json2yaml/releases/download/v0.1.2/json2yaml_v0.1.2_linux_amd64.tar.gz'
-      sha256 'e9e5c710b487ffde23bba947535d8bd968d01cc9d6aff6a8214a64844ca73847'
+      url 'https://github.com/itchyny/json2yaml/releases/download/v0.1.3/json2yaml_v0.1.3_linux_amd64.tar.gz'
+      sha256 '34bbfabb857375445d34f725f33e58783b6b58bf8c72841e72649cb2b0969093'
     end
   end
 
--- README.md
+++ README.md
@@ -7,7 +7,7 @@
 - [formulate](https://github.com/itchyny/formulate) 0.0.7
 - [git-branch-name](https://github.com/itchyny/git-branch-name) 0.0.2
 - [gojo](https://github.com/itchyny/gojo) 0.3.0
-- [json2yaml](https://github.com/itchyny/json2yaml) 0.1.2
+- [json2yaml](https://github.com/itchyny/json2yaml) 0.1.3
 - [mackerel-plugin-battery](https://github.com/itchyny/mackerel-plugin-battery) 0.1.0
 - [mackerel-plugin-dice](https://github.com/itchyny/mackerel-plugin-dice) 0.0.0
 - [mackerel-plugin-uptime](https://github.com/itchyny/mackerel-plugin-uptime) 0.2.0
