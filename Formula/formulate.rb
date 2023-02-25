5b46eb8533a8f803a5acf93913a73f02d5c18417
itchyny <itchyny@cybozu.co.jp>
2023-02-25 14:18:06 +0900

    formulate v0.0.10

--- Formula/formulate.rb
+++ Formula/formulate.rb
@@ -1,8 +1,8 @@
 class Formulate < Formula
   homepage 'https://github.com/itchyny/formulate'
-  version '0.0.9'
-  url 'https://github.com/itchyny/formulate/archive/v0.0.9.zip'
-  sha256 'fa335983b6cb59e459fac55a86eb036bcb8c3b6bc3c347b7ee10ae26a6f666f6'
+  version '0.0.10'
+  url 'https://github.com/itchyny/formulate/archive/v0.0.10.zip'
+  sha256 '5b12b5918742299b3dddc592766f58040515c552daef0393cf476c2d88ec2012'
 
   def install
     bin.install 'formulate'
