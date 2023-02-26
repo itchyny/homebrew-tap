766e5b900d6b5cb2e71c68f21366472e961d34ca
itchyny <itchyny@cybozu.co.jp>
2023-02-26 17:44:47 +0900

    base58 v0.2.1

--- Formula/base58.rb
+++ Formula/base58.rb
@@ -1,22 +1,22 @@
 class Base58 < Formula
   homepage 'https://github.com/itchyny/base58-go'
-  version '0.2.0'
+  version '0.2.1'
 
   if OS.mac?
     if Hardware::CPU.arm?
-      url 'https://github.com/itchyny/base58-go/releases/download/v0.2.0/base58_v0.2.0_darwin_arm64.zip'
-      sha256 'b3e92ec8330be1c306c25cdb472d15660cc5f8c7ee6d971f59fb0b1031554517'
+      url 'https://github.com/itchyny/base58-go/releases/download/v0.2.1/base58_v0.2.1_darwin_arm64.zip'
+      sha256 '23fca079e416d5db9595e0a929ada44d4fe19292b9299822f7704114047b0cfc'
     elsif Hardware::CPU.intel?
-      url 'https://github.com/itchyny/base58-go/releases/download/v0.2.0/base58_v0.2.0_darwin_amd64.zip'
-      sha256 '98c0fa3c4def0486603f84214aa615af550b25fab82c53141e067f74a0cf62ce'
+      url 'https://github.com/itchyny/base58-go/releases/download/v0.2.1/base58_v0.2.1_darwin_amd64.zip'
+      sha256 '0ab3d179f9aae5da3f6768ceeedb7fecd29cc77d25d215a8be6fab8a31cd4f87'
     end
   elsif OS.linux?
     if Hardware::CPU.arm?
-      url 'https://github.com/itchyny/base58-go/releases/download/v0.2.0/base58_v0.2.0_linux_arm64.tar.gz'
-      sha256 '2f773520356fbc6cf91cf272e748e3fae1b60b06f4b7063d6dc6c5262192c290'
+      url 'https://github.com/itchyny/base58-go/releases/download/v0.2.1/base58_v0.2.1_linux_arm64.tar.gz'
+      sha256 '8a714e0f8323234104bb3b4f3803d186481bee6f9191740586050ce51a6e87c9'
     elsif Hardware::CPU.intel?
-      url 'https://github.com/itchyny/base58-go/releases/download/v0.2.0/base58_v0.2.0_linux_amd64.tar.gz'
-      sha256 'e27910834ce01a445ed157a6fbf1cc2aaf27141672874eb584f2ceb3adacc9b0'
+      url 'https://github.com/itchyny/base58-go/releases/download/v0.2.1/base58_v0.2.1_linux_amd64.tar.gz'
+      sha256 '2185c2132700faacb4e0e3ec08ee9277e07a6d8ba4ce5789b34d0cc457dbacaf'
     end
   end
 
