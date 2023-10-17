c52fd9687f141f6fbf1b473bc3a6db57fda6e987
itchyny <itchyny@cybozu.co.jp>
2023-10-17 21:33:17 +0900

    mackerel-plugin-battery v0.2.0

--- Formula/mackerel-plugin-battery.rb
+++ Formula/mackerel-plugin-battery.rb
@@ -1,22 +1,22 @@
 class MackerelPluginBattery < Formula
   homepage 'https://github.com/itchyny/mackerel-plugin-battery'
-  version '0.1.0'
+  version '0.2.0'
 
   if OS.mac?
     if Hardware::CPU.arm?
-      url 'https://github.com/itchyny/mackerel-plugin-battery/releases/download/v0.1.0/mackerel-plugin-battery_v0.1.0_darwin_arm64.zip'
-      sha256 'f43be04e0c757f2555902120f7792f0d4b2a2f584838973bf1d93dc240da3a20'
+      url 'https://github.com/itchyny/mackerel-plugin-battery/releases/download/v0.2.0/mackerel-plugin-battery_v0.2.0_darwin_arm64.zip'
+      sha256 'd47916c2370deb37b3f82ce06c0bbd2ec3d40985294018844ffe586e02b98cdd'
     elsif Hardware::CPU.intel?
-      url 'https://github.com/itchyny/mackerel-plugin-battery/releases/download/v0.1.0/mackerel-plugin-battery_v0.1.0_darwin_amd64.zip'
-      sha256 'a517ee8f70d74aef4bd5dba739baf871af05f966b0c8b1e2b9e33257bb8b4fb8'
+      url 'https://github.com/itchyny/mackerel-plugin-battery/releases/download/v0.2.0/mackerel-plugin-battery_v0.2.0_darwin_amd64.zip'
+      sha256 '1de822d9dd14c6554ec0f6d3f9e9280bcd521f38580c5536ee39f55c9378dd96'
     end
   elsif OS.linux?
     if Hardware::CPU.arm?
-      url 'https://github.com/itchyny/mackerel-plugin-battery/releases/download/v0.1.0/mackerel-plugin-battery_v0.1.0_linux_arm64.tar.gz'
-      sha256 '76dc24a9f9de38338a899d0981133b02d383a7ce3c902d2deb60e92569803d53'
+      url 'https://github.com/itchyny/mackerel-plugin-battery/releases/download/v0.2.0/mackerel-plugin-battery_v0.2.0_linux_arm64.tar.gz'
+      sha256 '91595a78ad6038085ca0db3a932f7f9170e0812a3909b6daebab4c576738efdb'
     elsif Hardware::CPU.intel?
-      url 'https://github.com/itchyny/mackerel-plugin-battery/releases/download/v0.1.0/mackerel-plugin-battery_v0.1.0_linux_amd64.tar.gz'
-      sha256 '62c8a31df650309110b0f6a0db2d117e18c0f4a1d85bae914714c1f528f76fdb'
+      url 'https://github.com/itchyny/mackerel-plugin-battery/releases/download/v0.2.0/mackerel-plugin-battery_v0.2.0_linux_amd64.tar.gz'
+      sha256 '5eaa3cc9842dbe5652ce7895d0cf9af06ae698cbc0c7af10a8433606d978b597'
     end
   end
 
