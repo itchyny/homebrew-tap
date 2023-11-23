4bd095ca03d4c5af9f35584c2162fd8efd03ea5c
itchyny <itchyny@cybozu.co.jp>
2023-11-23 16:51:07 +0900

    mackerel-plugin-uptime v0.2.2

--- Formula/mackerel-plugin-uptime.rb
+++ Formula/mackerel-plugin-uptime.rb
@@ -1,21 +1,21 @@
 class MackerelPluginUptime < Formula
   homepage 'https://github.com/itchyny/mackerel-plugin-uptime'
-  version '0.2.1'
+  version '0.2.2'
 
   if OS.mac?
     if Hardware::CPU.arm?
-      url 'https://github.com/itchyny/mackerel-plugin-uptime/releases/download/v0.2.1/mackerel-plugin-uptime_v0.2.1_darwin_arm64.zip'
-      sha256 'ca026106524f617867a5ce26f9f9d660d190c98679638b52210518402b676852'
+      url 'https://github.com/itchyny/mackerel-plugin-uptime/releases/download/v0.2.2/mackerel-plugin-uptime_v0.2.2_darwin_arm64.zip'
+      sha256 'b1c8df771cac3f57a71d49c93c8ff4ac6cf55df175886c1247eeaabc9156675c'
     elsif Hardware::CPU.intel?
-      url 'https://github.com/itchyny/mackerel-plugin-uptime/releases/download/v0.2.1/mackerel-plugin-uptime_v0.2.1_darwin_amd64.zip'
-      sha256 '7d9ef7572691d22a9cb1ceb8b5220f8eeee0eac1047b703d3f0e7f7c96489791'
+      url 'https://github.com/itchyny/mackerel-plugin-uptime/releases/download/v0.2.2/mackerel-plugin-uptime_v0.2.2_darwin_amd64.zip'
+      sha256 '92be251f8980121631a17bbba4bfbecd37e1798e0c1dc5d7ef34e24ae1044c43'
     end
   elsif OS.linux?
     if Hardware::CPU.arm?
-      url 'https://github.com/itchyny/mackerel-plugin-uptime/releases/download/v0.2.1/mackerel-plugin-uptime_v0.2.1_linux_arm64.tar.gz'
+      url 'https://github.com/itchyny/mackerel-plugin-uptime/releases/download/v0.2.2/mackerel-plugin-uptime_v0.2.2_linux_arm64.tar.gz'
       sha256 'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855'
     elsif Hardware::CPU.intel?
-      url 'https://github.com/itchyny/mackerel-plugin-uptime/releases/download/v0.2.1/mackerel-plugin-uptime_v0.2.1_linux_amd64.tar.gz'
+      url 'https://github.com/itchyny/mackerel-plugin-uptime/releases/download/v0.2.2/mackerel-plugin-uptime_v0.2.2_linux_amd64.tar.gz'
       sha256 'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855'
     end
   end
