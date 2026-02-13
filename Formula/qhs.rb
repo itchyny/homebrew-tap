d233680c6ccd3b192ca6390d532c17471df91d31
itchyny <itchyny@cybozu.co.jp>
2026-02-13 17:53:43 +0900

    qhs v0.4.2

--- Formula/qhs.rb
+++ Formula/qhs.rb
@@ -1,13 +1,13 @@
 class Qhs < Formula
   homepage 'https://github.com/itchyny/qhs'
-  version '0.4.1'
+  version '0.4.2'
 
   if OS.mac?
-    url 'https://github.com/itchyny/qhs/releases/download/v0.4.1/qhs_v0.4.1_darwin_arm64.zip'
-    sha256 '4f47d99175cf24600db8772a55e3dc07abf679c0b7948645617545fc19fae004'
+    url 'https://github.com/itchyny/qhs/releases/download/v0.4.2/qhs_v0.4.2_darwin_arm64.zip'
+    sha256 '525d4ca2f05c81916cc7888638b5041a0a6e2bbdc5c67a4c093739114069d9e6'
   elsif OS.linux?
-    url 'https://github.com/itchyny/qhs/releases/download/v0.4.1/qhs_v0.4.1_linux_amd64.zip'
-    sha256 'c4361fb15a3f8966616d0b9cae840ee59329a47de9daafd2974eae0233e8b963'
+    url 'https://github.com/itchyny/qhs/releases/download/v0.4.2/qhs_v0.4.2_linux_amd64.zip'
+    sha256 'c2a3845f4480068c1ffdce0eff07fb6d547a945277878f46fcf6928edc081f43'
   end
 
   def install
